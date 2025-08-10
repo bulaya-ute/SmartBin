25a3e315809880420c53624e2505155b9fb70751
bulaya-ute <bulayamwanaute7@gmail.com>
2025-08-10 04:30:38 +0200

    Beginning works on arduino code

--- model_test.py
+++ model_test.py
@@ -1,6 +1,8 @@
 from ultralytics import YOLO
 
 # model = YOLO("yolo11n-seg.pt")
-model = YOLO("runs/train3/weights/best.pt")
+# model = YOLO("runs/train3/weights/best.pt")
+# model = YOLO("/home/bulaya/Downloads/Programs/runs-20250730T233253Z-1-001/runs/segment/train/weights/best.pt")
+model = YOLO("best.pt")
 
 model.predict(0, show=True)
